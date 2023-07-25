6f4da77978a62dce9bbd909662a9df94edc371fa
NavinGokul27 <138608001+NavinGokul27@users.noreply.github.com>
2023-07-25 19:48:19 +0530

    Add files via upload

---
+++ rank.py
@@ -0,0 +1,11 @@
+a=int(input( Enter your marks));
+
+
+if(marks>90):
+ print("you are grade A");
+elif(marks>75):
+ print("you are grade B");
+elif(marks>65):
+ print("you are grade C");
+else:
+ print(you are fail);
